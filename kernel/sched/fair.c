ddd161e3d1420166d2d07eea22efda3604fcf2e1
Brendan Jackman <brendan.jackman@arm.com>
2017-01-09 17:20:11 +0000

    DEBUG: sched/fair: Fix missing sched_load_avg_cpu events
    update_cfs_rq_load_avg is called from update_blocked_averages without triggering
    the sched_load_avg_cpu event. Move the event trigger to inside
    update_cfs_rq_load_avg to avoid this missing event.
    
    Change-Id: I6c4f66f687a644e4e7f798db122d28a8f5919b7b
    Signed-off-by: Brendan Jackman <brendan.jackman@arm.com>
    (cherry picked from commit 7f18f0963d81a096e741cfc14ec9c2915f633e0a)
    (cherry picked from commit 998d092b3385f1e62fa5cb5f491c7c633a4bbc3e)
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3084,6 +3084,8 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 	if (update_freq && (decayed || removed_util))
 		cfs_rq_util_change(cfs_rq);
 
+	trace_sched_load_avg_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
+
 	return decayed || removed_load;
 }
 
@@ -3108,7 +3110,6 @@ static inline void update_load_avg(struct sched_entity *se, int update_tg)
 
 	if (entity_is_task(se))
 		trace_sched_load_avg_task(task_of(se), &se->avg);
-	trace_sched_load_avg_cpu(cpu, cfs_rq);
 }
 
 /**
