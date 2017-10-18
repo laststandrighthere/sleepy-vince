be9fd83e15f47fdbc554606366b370a0981a4cf5
Justinien Bouron <justinien.bouron@epfl.ch>
2017-10-18 13:00:15 +0200

    Fix
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2421,7 +2421,10 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
 	} else if (rt_prio(p->prio)) {
 		p->sched_class = &rt_sched_class;
 	} else {
-		p->sched_class = &ktz_sched_class;
+		if (ktz_prio(p->prio)) 
+			p->sched_class = &ktz_sched_class;
+		else
+			p->sched_class = &fair_sched_class;
 	}
 
 	init_entity_runnable_average(&p->se);
@@ -3721,7 +3724,10 @@ void rt_mutex_setprio(struct task_struct *p, int prio)
 			p->dl.dl_boosted = 0;
 		if (rt_prio(oldprio))
 			p->rt.timeout = 0;
-		p->sched_class = &ktz_sched_class;
+		if (ktz_prio(prio))
+			p->sched_class = &ktz_sched_class;
+		else
+			p->sched_class = &fair_sched_class;
 	}
 
 	p->prio = prio;
@@ -3778,6 +3784,12 @@ void set_user_nice(struct task_struct *p, long nice)
 	p->prio = effective_prio(p);
 	delta = p->prio - old_prio;
 
+	const struct sched_class *prev_class = p->sched_class;
+	if (ktz_prio(p->prio))
+		p->sched_class = &ktz_sched_class;
+	else
+		p->sched_class = &fair_sched_class;
+
 	if (queued) {
 		enqueue_task(rq, p, ENQUEUE_RESTORE);
 		/*
@@ -3790,7 +3802,7 @@ void set_user_nice(struct task_struct *p, long nice)
 	if (running)
 		set_curr_task(rq, p);
 
-	check_class_changed(rq, p, &ktz_sched_class, old_prio);
+	check_class_changed(rq, p, prev_class, old_prio);
 out_unlock:
 	task_rq_unlock(rq, p, &rf);
 }
@@ -3995,8 +4007,12 @@ static void __setscheduler(struct rq *rq, struct task_struct *p,
 		p->sched_class = &dl_sched_class;
 	else if (rt_prio(p->prio))
 		p->sched_class = &rt_sched_class;
-	else
-		p->sched_class = &ktz_sched_class;
+	else {
+		if (ktz_prio(p->prio))
+			p->sched_class = &ktz_sched_class;
+		else
+			p->sched_class = &fair_sched_class;
+	}
 }
 
 static void
