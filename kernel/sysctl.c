688b500eb402a3e056e172be96d999a48d8152f0
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-01-09 11:02:30 +0530

    sched: Add trace point to track preemption disable callers
    Add trace point to track preemption disable callers to
    isolate issues unrelated to scheduler and improve debug
    turn around time.
    
    Change-Id: If9303b7165167e8f79cd339929daf4afc31a61c4
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -291,7 +291,8 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#if defined(CONFIG_PREEMPT_TRACER) || defined(CONFIG_IRQSOFF_TRACER)
+#if defined(CONFIG_DEBUG_PREEMPT) || defined(CONFIG_PREEMPT_TRACER) || \
+					defined(CONFIG_PREEMPTIRQ_EVENTS)
 	{
 		.procname       = "preemptoff_tracing_threshold_ns",
 		.data           = &sysctl_preemptoff_tracing_threshold_ns,
