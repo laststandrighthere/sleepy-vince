dba7d8bb921bf01e1bef5f83f9b9044bb52f1c96
Ingo Molnar <mingo@kernel.org>
2018-03-03 16:27:54 +0100

    sched/deadline, rt: Rename queue_push_tasks/queue_pull_task to create separate namespace
    There are similarly named functions in both of these modules:
    
      kernel/sched/deadline.c:static inline void queue_push_tasks(struct rq *rq)
      kernel/sched/deadline.c:static inline void queue_pull_task(struct rq *rq)
      kernel/sched/deadline.c:static inline void queue_push_tasks(struct rq *rq)
      kernel/sched/deadline.c:static inline void queue_pull_task(struct rq *rq)
      kernel/sched/deadline.c:	queue_push_tasks(rq);
      kernel/sched/deadline.c:	queue_pull_task(rq);
      kernel/sched/deadline.c:			queue_push_tasks(rq);
      kernel/sched/deadline.c:			queue_pull_task(rq);
      kernel/sched/rt.c:static inline void queue_push_tasks(struct rq *rq)
      kernel/sched/rt.c:static inline void queue_pull_task(struct rq *rq)
      kernel/sched/rt.c:static inline void queue_push_tasks(struct rq *rq)
      kernel/sched/rt.c:	queue_push_tasks(rq);
      kernel/sched/rt.c:	queue_pull_task(rq);
      kernel/sched/rt.c:			queue_push_tasks(rq);
      kernel/sched/rt.c:			queue_pull_task(rq);
    
    ... which makes it harder to grep for them. Prefix them with
    deadline_ and rt_, respectively.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -513,7 +513,7 @@ static DEFINE_PER_CPU(struct callback_head, dl_pull_head);
 static void push_dl_tasks(struct rq *);
 static void pull_dl_task(struct rq *);
 
-static inline void queue_push_tasks(struct rq *rq)
+static inline void deadline_queue_push_tasks(struct rq *rq)
 {
 	if (!has_pushable_dl_tasks(rq))
 		return;
@@ -521,7 +521,7 @@ static inline void queue_push_tasks(struct rq *rq)
 	queue_balance_callback(rq, &per_cpu(dl_push_head, rq->cpu), push_dl_tasks);
 }
 
-static inline void queue_pull_task(struct rq *rq)
+static inline void deadline_queue_pull_task(struct rq *rq)
 {
 	queue_balance_callback(rq, &per_cpu(dl_pull_head, rq->cpu), pull_dl_task);
 }
@@ -596,11 +596,11 @@ static inline void pull_dl_task(struct rq *rq)
 {
 }
 
-static inline void queue_push_tasks(struct rq *rq)
+static inline void deadline_queue_push_tasks(struct rq *rq)
 {
 }
 
-static inline void queue_pull_task(struct rq *rq)
+static inline void deadline_queue_pull_task(struct rq *rq)
 {
 }
 #endif /* CONFIG_SMP */
@@ -1762,7 +1762,7 @@ pick_next_task_dl(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 	if (hrtick_enabled(rq))
 		start_hrtick_dl(rq, p);
 
-	queue_push_tasks(rq);
+	deadline_queue_push_tasks(rq);
 
 	return p;
 }
@@ -2304,7 +2304,7 @@ static void switched_from_dl(struct rq *rq, struct task_struct *p)
 	if (!task_on_rq_queued(p) || rq->dl.dl_nr_running)
 		return;
 
-	queue_pull_task(rq);
+	deadline_queue_pull_task(rq);
 }
 
 /*
@@ -2326,7 +2326,7 @@ static void switched_to_dl(struct rq *rq, struct task_struct *p)
 	if (rq->curr != p) {
 #ifdef CONFIG_SMP
 		if (p->nr_cpus_allowed > 1 && rq->dl.overloaded)
-			queue_push_tasks(rq);
+			deadline_queue_push_tasks(rq);
 #endif
 		if (dl_task(rq->curr))
 			check_preempt_curr_dl(rq, p, 0);
@@ -2351,7 +2351,7 @@ static void prio_changed_dl(struct rq *rq, struct task_struct *p,
 		 * or lowering its prio, so...
 		 */
 		if (!rq->dl.overloaded)
-			queue_pull_task(rq);
+			deadline_queue_pull_task(rq);
 
 		/*
 		 * If we now have a earlier deadline task than p,
