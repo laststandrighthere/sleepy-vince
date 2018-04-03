ee7e1f350121d43d82e9d5d28813ed98e0e7c781
Viresh Kumar <viresh.kumar@linaro.org>
2018-04-03 15:37:39 +0530

    cpufreq: Drop cpufreq_table_validate_and_show()
    This isn't used anymore. Remove the helper and update documentation
    accordingly.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -946,8 +946,6 @@ extern void arch_set_max_freq_scale(struct cpumask *cpus,
 extern struct freq_attr cpufreq_freq_attr_scaling_available_freqs;
 extern struct freq_attr cpufreq_freq_attr_scaling_boost_freqs;
 extern struct freq_attr *cpufreq_generic_attr[];
-int cpufreq_table_validate_and_show(struct cpufreq_policy *policy,
-				      struct cpufreq_frequency_table *table);
 int cpufreq_table_validate_and_sort(struct cpufreq_policy *policy);
 
 unsigned int cpufreq_generic_get(unsigned int cpu);
