8b33fff85d41791cee6d1f2923c1e6a478764ca8
Pavankumar Kondeti <pkondeti@codeaurora.org>
2018-04-03 19:34:03 +0530

    sched: Fix compilation issues with schedutil for !SCHED_WALT
    sched_ravg_window and sysctl_sched_use_walt_cpu_util are WALT specific
    tunables. These are used outside !SCHED_WALT in schedutil. Add stubs
    for these to fix compilation with WALT disabled.
    
    Change-Id: I3f61cf06a857d52da5eda1d51701ad7cc21598d3
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2026,6 +2026,9 @@ cpu_util_freq(int cpu, struct sched_walt_cpu_load *walt_load)
 	return cpu_util(cpu);
 }
 
+#define sched_ravg_window TICK_NSEC
+#define sysctl_sched_use_walt_cpu_util 0
+
 #endif /* CONFIG_SCHED_WALT */
 
 extern unsigned int capacity_margin_freq;
