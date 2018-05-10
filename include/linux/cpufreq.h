bbe97246dde6e3ec2f45bef0b2e02bdef18eec94
Dietmar Eggemann <dietmar.eggemann@arm.com>
2018-05-10 16:52:33 +0100

    ANDROID: implement max frequency capping
    Implements the Max Frequency Capping Engine (MFCE) getter function
    topology_get_max_freq_scale() to provide the scheduler with a
    maximum frequency scaling correction factor for more accurate cpu
    capacity handling by being able to deal with max frequency capping.
    
    This scaling factor describes the influence of running a cpu with a
    current maximum frequency (policy) lower than the maximum possible
    frequency (cpuinfo).
    
    The factor is:
    
      policy_max_freq(cpu) << SCHED_CAPACITY_SHIFT / cpuinfo_max_freq(cpu)
    
    It also implements the MFCE setter function arch_set_max_freq_scale()
    which is called from cpufreq_set_policy().
    
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    [Trivial cherry-pick issue in cpufreq.c]
    Signed-off-by: Quentin Perret <quentin.perret@arm.com>
    Change-Id: I59e52861ee260755ab0518fe1f7183a2e4e3d0fc
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -892,6 +892,8 @@ static inline bool policy_has_boost_freq(struct cpufreq_policy *policy)
 
 extern void arch_set_freq_scale(struct cpumask *cpus, unsigned long cur_freq,
 				unsigned long max_freq);
+extern void arch_set_max_freq_scale(struct cpumask *cpus,
+				    unsigned long policy_max_freq);
 
 /* the following are really really optional */
 extern struct freq_attr cpufreq_freq_attr_scaling_available_freqs;
