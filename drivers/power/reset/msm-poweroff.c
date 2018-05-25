5b237894c94f0c942bc191ad85b9c9e837c90f27
Chris Fries <cfries@google.com>
2018-05-25 15:02:18 -0500

    power: reset: Add "warm_reset" parameter
    Add a module parameter named "warm_reset" that can override the
    default cold-reset behavior of the SoC.
    
    Bug: 79161072
    Change-Id: I02659c28f2446d77deab31035c968ade30e68e94
    Signed-off-by: Chris Fries <cfries@google.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -88,6 +88,10 @@ static bool scm_dload_supported;
 static struct kobject dload_kobj;
 static void *dload_type_addr;
 
+static bool warm_reset;
+module_param(warm_reset, bool, 0644);
+MODULE_PARM_DESC(warm_reset, "Set 1 to override default cold-reset");
+
 static int dload_set(const char *val, const struct kernel_param *kp);
 /* interface for exporting attributes */
 struct reset_attribute {
@@ -285,7 +289,7 @@ static void halt_spmi_pmic_arbiter(void)
 
 static void msm_restart_prepare(const char *cmd)
 {
-	bool need_warm_reset = false;
+	bool need_warm_reset = warm_reset;
 #ifdef CONFIG_QCOM_DLOAD_MODE
 	/* Write download mode flags if we're panic'ing
 	 * Write download mode flags if restart_mode says so
@@ -302,9 +306,8 @@ static void msm_restart_prepare(const char *cmd)
 			((cmd != NULL && cmd[0] != '\0') &&
 			!strcmp(cmd, "edl")))
 			need_warm_reset = true;
-	} else {
-		need_warm_reset = (get_dload_mode() ||
-				(cmd != NULL && cmd[0] != '\0'));
+	} else if (get_dload_mode() || (cmd != NULL && cmd[0] != '\0')) {
+		need_warm_reset = true;
 	}
 
 	if (force_warm_reboot)
