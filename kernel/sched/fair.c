25c33e517b1790504c84f791221ba51490812de4
Chris Redpath <chris.redpath@arm.com>
2018-06-08 17:16:50 +0100

    FIXUP: ANDROID: sched/fair: return idle CPU immediately for prefer_idle
    When a CPU is selected for a prefer_idle task through find_best_target
    this CPU will become a target CPU on which we perform an energy diff and
    select between the previous CPU and the target CPU based on the estimated
    energy consumption for both placements.
    
    For prefer_idle tasks we should favour performance over energy savings
    and therefore return a found idle CPU immediately.
    
    Change-Id: I2242a51134ac172dd58b3b08375388a7b4b84af0
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    Suggested-by: Joel Fernandes <joelaf@google.com>
    Git-commit: dc6f7513623fe5fb4be5018cf334caee5b9ca039
    Git-repo: http://android.googlesource.com/kernel/common
    [adharmap@codeaurora.org: Continue to return prev_cpu instead of -1
    in case we cannot find a suitable cpu to place the task. The variable
    energy_cpu is kept around for just that.]
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8087,7 +8087,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 {
 	int use_fbt = sched_feat(FIND_BEST_TARGET);
 	int cpu_iter, eas_cpu_idx = EAS_CPU_NXT;
-	int delta = 0;
+	int energy_cpu = prev_cpu, delta = 0;
 	int target_cpu = -1;
 	struct energy_env *eenv;
 	struct cpumask *rtg_target = find_rtg_target(p);
@@ -8176,8 +8176,10 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 					      boosted, prefer_idle, &fbt_env);
 
 		/* Immediately return a found idle CPU for a prefer_idle task */
-		if (prefer_idle && target_cpu >= 0 && idle_cpu(target_cpu))
-			return target_cpu;
+		if (prefer_idle && target_cpu >= 0 && idle_cpu(target_cpu)) {
+			energy_cpu = target_cpu;
+			goto out;
+		}
 
 		/* Place target into NEXT slot */
 		eenv->cpu[EAS_CPU_NXT].cpu_id = target_cpu;
