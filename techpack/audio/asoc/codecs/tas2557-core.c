608d5c24e6a7300948a57cb4e1a7ab9358cd5edb
Tracy Yi <tracy-yi@ti.com>
2018-11-09 17:30:05 +0800

    ASoC: tas2557: Reload FW after i2c recover
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- techpack/audio/asoc/codecs/tas2557-core.c
+++ techpack/audio/asoc/codecs/tas2557-core.c
@@ -326,6 +326,8 @@ static void failsafe(struct tas2557_priv *pTAS2557)
 
 	if (pTAS2557->mpFirmware != NULL)
 		tas2557_clear_firmware(pTAS2557->mpFirmware);
+
+	pTAS2557->mpFirmware->mnPrograms = 0;
 }
 
 int tas2557_checkPLL(struct tas2557_priv *pTAS2557)
