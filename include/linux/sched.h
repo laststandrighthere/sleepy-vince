c45ec9ea3e0de12e9ec6952ae5b9cf09f9c58a27
kdrag0n <dragon@khronodragon.com>
2019-01-12 15:39:15 -0800

    sched/tune: introduce direct set_stune_boost()
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3966,6 +3966,7 @@ int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
 int do_prefer_idle(char *st_name, u64 prefer_idle);
+int set_stune_boost(char *st_name, int boost, int *boost_default);
 #else /* !CONFIG_DYNAMIC_STUNE_BOOST */
 static inline int do_stune_boost(char *st_name, int boost, int *slot)
 {
@@ -3983,6 +3984,11 @@ static inline int reset_stune_boost(char *st_name, int slot)
 }
 
 static inline int do_prefer_idle(char *st_name, u64 prefer_idle)
+{
+        return 0;
+}
+
+static inline int set_stune_boost(char *st_name, int boost, int *boost_default)
 {
 	return 0;
 }
