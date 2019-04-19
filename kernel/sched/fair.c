24c0f961a1bcee1b9136d130ad2d299825fb598c
Rick Yiu <rickyiu@google.com>
2019-04-19 20:33:57 +0800

    sched/fair: prefer exclusive max cluster cpu for top-app task
    For a top-app task, if it fits in max cluster, then prefer the first
    cpu in this cluster whenever possible, which happens to be the top-app
    exclusive cpu in our cpuset design.
    
    Bug: 128477368
    Test: top-app tasks assigned as expected
    Change-Id: Ifdd0614f6c8c03edde4ed674c4193f4ba31aac16
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    [Lau: switched from mid cluster to max cluster]
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7501,6 +7501,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	unsigned int active_cpus_count = 0;
 	int prev_cpu = task_cpu(p);
 	bool next_group_higher_cap = false;
+	int max_cap_orig_cpu = cpu_rq(smp_processor_id())->rd->max_cap_orig_cpu;
 
 	*backup_cpu = -1;
 
@@ -7653,11 +7654,14 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				/*
 				 * Case A.1: IDLE CPU
 				 * Return the best IDLE CPU we find:
-				 * - for boosted tasks: the CPU with the highest
-				 * performance (i.e. biggest capacity_orig)
+				 * - for boosted tasks: if the task fits in max
+				 * cluster, prefer the first max cluster cpu.
 				 * - for !boosted tasks: the most energy
 				 * efficient CPU (i.e. smallest capacity_orig)
 				 */
+				if (boosted && max_cap_orig_cpu != -1 &&
+					best_idle_cpu == max_cap_orig_cpu)
+					break;
 				if (idle_cpu(i)) {
 					if (boosted &&
 					    capacity_orig < target_capacity)
@@ -7715,11 +7719,11 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 				/*
 				 * If utilization is the same between CPUs,
-				 * break the ties with WALT's cumulative
-				 * demand
+				 * break the ties with cumulative demand,
+				 * also prefer lower order cpu.
 				 */
 				if (new_util == best_active_util &&
-				    new_util_cuml > best_active_cuml_util)
+					new_util_cuml >= best_active_cuml_util)
 					continue;
 				min_wake_util = wake_util;
 				best_active_util = new_util;
@@ -7850,7 +7854,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		if ((prefer_idle && best_idle_cpu != -1) ||
 		    (boosted && (best_idle_cpu != -1 || target_cpu != -1))) {
 			if (boosted) {
-				if (!next_group_higher_cap)
+				/*
+				 * For boosted task, stop searching when an idle
+				 * cpu is found in max cluster.
+				 */
+				if ((max_cap_orig_cpu != -1 &&
+					best_idle_cpu >= max_cap_orig_cpu) ||
+					!next_group_higher_cap)
 					break;
 			} else {
 				if (next_group_higher_cap)
