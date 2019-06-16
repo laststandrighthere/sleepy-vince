acd1771b890cd7698c5a8258751fc58324c32798
laststandrighthere <laststandrighthere@gmail.com>
2019-06-16 19:02:01 +0000

    techpack: wsa881x-analog: avoid VLA
    Signed-off-by: laststandrighthere <laststandrighthere@gmail.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- techpack/audio/asoc/codecs/wsa881x-analog.c
+++ techpack/audio/asoc/codecs/wsa881x-analog.c
@@ -175,7 +175,7 @@ static int wsa881x_i2c_write_device(struct wsa881x_pdata *wsa881x,
 	int ret = 0;
 	int bytes = 1;
 	u8 reg_addr = 0;
-	u8 data[bytes + 1];
+	u8 data[2];
 
 	wsa881x_index = get_i2c_wsa881x_device_index(reg);
 	if (wsa881x_index < 0) {
