f7081af04f20f5199d0e5f83094650d7824e3fc8
Con Kolivas <kernel@kolivas.org>
2019-07-20 17:35:01 -0500

    MuQSS: Fix wake_cpu set
    Fixes wake_cpu not being set on running tasks that have
    sched_setaffinity called on them.
    
    Debugging and patch courtesy of Gottschlag, Mathias.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/MuQSS.c
+++ kernel/sched/MuQSS.c
@@ -5698,8 +5698,8 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	bool queued = false, running_wrong = false, kthread;
 	struct cpumask old_mask;
 	unsigned long flags;
+	int cpu, ret = 0;
 	struct rq *rq;
-	int ret = 0;
 
 	rq = task_rq_lock(p, &flags);
 
@@ -5749,12 +5749,14 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	if (task_running(rq, p)) {
 		/* Task is running on the wrong cpu now, reschedule it. */
 		if (rq == this_rq()) {
+			cpu = cpumask_any_and(cpu_valid_mask, new_mask);
+			set_task_cpu(p, cpu);
 			set_tsk_need_resched(p);
 			running_wrong = kthread;
 		} else
 			resched_task(p);
 	} else {
-		int cpu = cpumask_any_and(cpu_valid_mask, new_mask);
+		cpu = cpumask_any_and(cpu_valid_mask, new_mask);
 
 		if (queued) {
 			/*
