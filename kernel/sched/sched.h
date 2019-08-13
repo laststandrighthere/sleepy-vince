dd063c29476b119a2a4831f43f950816499192e0
Maria Yu <aiquny@codeaurora.org>
2019-08-13 17:12:33 +0800

    sched/walt: Avoid walt irq work in offlined cpu
    Avoid walt irq work in offlined cpu.
    
    Change-Id: Ia4410562f66bfa57daa15d8c0a785a2c7a95f2a0
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3060,3 +3060,13 @@ static inline bool energy_aware(void)
 {
 	return sched_feat(ENERGY_AWARE);
 }
+
+#ifdef CONFIG_SMP
+static inline void sched_irq_work_queue(struct irq_work *work)
+{
+	if (likely(cpu_online(raw_smp_processor_id())))
+		irq_work_queue(work);
+	else
+		irq_work_queue_on(work, cpumask_any(cpu_online_mask));
+}
+#endif
