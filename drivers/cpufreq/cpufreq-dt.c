d9a6088ef74d5c3897d29ac5df2f8b8db41cbb24
Lau <laststandrighthere@gmail.com>
2020-04-07 03:29:00 +0800

    Revert "ANDROID: cpufreq-dt: Set sane defaults for schedutil rate limits"
    Missed to remove in 4.9 EAS removal
    
    This reverts commit 28b159744f768d605de8fc9d109c0c46c25df893.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/cpufreq/cpufreq-dt.c
+++ drivers/cpufreq/cpufreq-dt.c
@@ -283,13 +283,6 @@ static int cpufreq_init(struct cpufreq_policy *policy)
 	policy->cpuinfo.transition_latency = transition_latency;
 	policy->dvfs_possible_from_any_cpu = true;
 
-        /*
-         * Android: set default parameters for parity between schedutil and
-         * schedfreq
-         */
-	policy->up_transition_delay_us = transition_latency / NSEC_PER_USEC;
-	policy->down_transition_delay_us = 50000; /* 50ms */
-
 	return 0;
 
 out_free_cpufreq_table:
