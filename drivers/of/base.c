4baaacdc63c6f9437fb8bdf26c6fb49f00254985
Lau <laststandrighthere@gmail.com>
2020-04-06 10:53:06 +0800

    of: base: Add missing include
    Missing #include <linux/of_device.h>
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/of/base.c
+++ drivers/of/base.c
@@ -27,6 +27,7 @@
 #include <linux/memblock.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/of_graph.h>
 #include <linux/spinlock.h>
 #include <linux/slab.h>
