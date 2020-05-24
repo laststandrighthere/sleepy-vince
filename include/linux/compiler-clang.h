4883ea7d1297281a11bb26c3017846a21b258629
Lau <laststandrighthere@gmail.com>
2020-05-24 10:14:19 +0800

    Revert "add support for clang Control Flow Integrity (CFI)"
    This reverts commit 00a195e7c0752ff5d65c9caadfbcc226270ca232.

--- include/linux/compiler-clang.h
+++ include/linux/compiler-clang.h
@@ -29,8 +29,6 @@
 #define __norecordmcount \
 	__attribute__((__section__(".text..ftrace")))
 #endif
-
-#define __nocfi		__attribute__((no_sanitize("cfi")))
 #endif
 
 /* all clang versions usable with the kernel support KASAN ABI version 5 */
