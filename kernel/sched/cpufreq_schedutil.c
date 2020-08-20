648ed9422929aade5bab1d5920d20c20e75c22f5
Lau <laststandrighthere@gmail.com>
2020-08-20 13:01:35 +0800

    cpufreq: schedutil: Remove stubs
    These were added in initial EAS bringup in order to reduce the
    amount of backports but now that We already have them implemented
    in 4.9, remove these stubs and use the actual code.
    
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -21,12 +21,6 @@
 
 unsigned long boosted_cpu_util(int cpu);
 
-/* Stub out fast switch routines present on mainline to reduce the backport
- * overhead. */
-#define cpufreq_driver_fast_switch(x, y) 0
-#define cpufreq_enable_fast_switch(x)
-#define cpufreq_disable_fast_switch(x)
-#define LATENCY_MULTIPLIER			(1000)
 #define SUGOV_KTHREAD_PRIORITY	50
 
 struct sugov_tunables {
